f640035a82ec7e2a95d65ba417d5a251ccb54825
AytorZ <azumbado80731@ufide.ac.cr>
2021-07-20 17:47:58 -0600

    Movimiento de patrulla

--- Segundo Examen/Assets/Scripts/PatrullaController.cs
+++ Segundo Examen/Assets/Scripts/PatrullaController.cs
@@ -5,21 +5,46 @@ using UnityEngine;
 public class PatrullaController : PsychicsObject
 {
     Animator animator;
+    bool facingRight = true;
+    bool toTheLeft = true;
+    int distanceCout = 800;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if(toTheLeft)
+        {
+            distanceCout--;
+            targetVelocity.x = -2F;
+        }
+        else
+        {
+            distanceCout--;
+            targetVelocity.x = 2F;
+        }
+
+        if(distanceCout == 0)
+        {
+            toTheLeft = !toTheLeft;
+            distanceCout = 1000;
+        }
+
+        if (targetVelocity.x != 0)
+        {
+            bool wasFacingRight = facingRight;
+            facingRight = targetVelocity.x < 0;
+
+            if (wasFacingRight != facingRight)
+            {
+                Vector3 localScale = transform.localScale;
+                localScale.x = -localScale.x;
+                transform.localScale = localScale;
+            }
+        }
     }
+
 }
