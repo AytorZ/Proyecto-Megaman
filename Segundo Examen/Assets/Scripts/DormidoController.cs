bc70f2aba06e6b3e9ff392fd9b3aac0d39c5b498
jjguzme <jjguzmanmeza15@gmail.com>
2021-07-20 16:08:24 -0600

    Enemigo dormido awakes

--- Segundo Examen/Assets/Scripts/DormidoController.cs
+++ Segundo Examen/Assets/Scripts/DormidoController.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DormidoController : MonoBehaviour
+public class DormidoController : MonoBehaviour 
 {
+    [SerializeField]
+    Transform hero;
+
+    //[SerializeField]
+    //float speed = 2F;
+
+    Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 move = Vector2.zero;
+        if (hero.position.x >= 1 && hero.position.y <= -2.11)
+        {
+            animator.SetBool("awake", true);
+            //velocity = move * speed;
+        }
     }
 }
