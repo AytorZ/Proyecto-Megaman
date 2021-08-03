8b859d0b271357ea5e49b98568de76cea4def840
AytorZ <azumbado80731@ufide.ac.cr>
2021-08-03 17:38:06 -0600

    Movimiento principal de MegaMan

---
+++ Juego Megaman/Assets/Scripts/MegaManController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MegaManController : PhysicsObject
+{
+    [SerializeField]
+    float maxSpeed = 1F;
+
+    [SerializeField]
+    float jumpTakeOffSpeed = 6F;
+
+    Animator animator;
+    bool facingRight = true;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+        move.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            animator.SetBool("isJumping", true);
+            velocity.y = jumpTakeOffSpeed;
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if (velocity.y > 0)
+            {
+                animator.SetBool("isJumping", false);
+                animator.SetBool("isFalling", true);
+                velocity.y = velocity.y * 0.5F;
+
+            }
+        }
+
+        if (grounded)
+        {
+            animator.SetBool("isFalling", false);
+        }
+
+        if (move.x != 0)
+        {
+            bool wasFacingRight = facingRight;
+            facingRight = move.x > 0;
+
+            if (wasFacingRight != facingRight)
+            {
+                Vector3 localScale = transform.localScale;
+                localScale.x = -localScale.x;
+                transform.localScale = localScale;
+            }
+        }
+
+        animator.SetFloat("Speed", Mathf.Abs(move.x));
+        targetVelocity = move * maxSpeed;
+    }
+}
---
+++ Juego Megaman/Assets/Scripts/PhysicsObject.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+    protected const float minMoveDistance = 0.001F;
+    protected const float shellRadius = 0.01F;
+
+    [SerializeField]
+    float gravityModifier = 1F;
+
+    [SerializeField]
+    float minGroundNormalY = 0.65F;
+
+
+    protected Vector2 groundNormal;
+    protected Vector2 velocity;
+    protected Vector2 targetVelocity;
+    protected Rigidbody2D rb;
+
+    protected bool grounded;
+
+    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>();
+    protected ContactFilter2D filter;
+
+    void Start()
+    {
+        filter.useTriggers = false;
+        filter.useLayerMask = true;
+        filter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+    }
+
+    void OnEnable()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetVelocity = Vector2.zero;
+        ComputeVelocity();
+    }
+
+    protected virtual void ComputeVelocity()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+        velocity.x = targetVelocity.x;
+        grounded = false;
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+        Vector2 moveAlongGround = new Vector2(groundNormal.y, -groundNormal.x);
+        Vector2 move = moveAlongGround * deltaPosition.x;
+        Movement(move, false);
+        move = Vector2.up * deltaPosition.y;
+        Movement(move, true);
+    }
+
+    void Movement(Vector2 move, bool yMovement)
+    {
+        float distance = move.magnitude;
+        if (distance > minMoveDistance)
+        {
+            int count = rb.Cast(move, filter, hitBuffer, distance + shellRadius);
+            hitBufferList.Clear();
+            for (int i = 0; i < count; i++)
+            {
+                hitBufferList.Add(hitBuffer[i]);
+            }
+            for (int i = 0; i < count; i++)
+            {
+                Vector2 currentNormal = hitBufferList[i].normal;
+                if (currentNormal.y > minGroundNormalY)
+                {
+                    grounded = true;
+                    if (yMovement)
+                    {
+                        groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+
+                float projection = Vector2.Dot(velocity, currentNormal);
+                if (projection < 0)
+                {
+                    velocity = velocity - projection * currentNormal;
+                }
+
+                float modifiedDistance = hitBufferList[i].distance - shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
+        }
+        rb.position = rb.position + move.normalized * distance;
+    }
+}
