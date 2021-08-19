fd4373ccfc733d4f9d7bd1c65a0b838e452671c1
jjguzme <jjguzmanmeza15@gmail.com>
2021-08-19 16:13:10 -0600

    Added scripts

---
+++ Juego Megaman/Assets/Scripts/CollisionController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionController : MonoBehaviour
+{
+    [SerializeField]
+    Transform character;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Destroy(character.gameObject);
+        }
+    }
+}
