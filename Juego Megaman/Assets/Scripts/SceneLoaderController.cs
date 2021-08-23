9d391b5372adc07c8b0254e410824b50c8a340f4
AytorZ <azumbado80731@ufide.ac.cr>
2021-08-22 19:46:20 -0600

    manejo de escenas

---
+++ Juego Megaman/Assets/Scripts/SceneLoaderController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderController : MonoBehaviour
+{
+    FaderController fader;
+
+    void Awake()
+    {
+        fader = FindObjectOfType<FaderController>();
+    }
+
+    void FadeAndLoadScene(int sceneIndex)
+    {
+        StartCoroutine(fader.FadeAndLoadScene(FaderController.FadeDirection.IN, sceneIndex));
+    }
+
+    /// <summary>
+    /// Carga la siguiente escena a partir de la escena actual.
+    /// </summary>
+    public void ProximaEscena()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        FadeAndLoadScene(currentScene + 1);
+    }
+
+    /// <summary>
+    /// Carga la primera escena del videojuego.
+    /// </summary>
+    public void PrimeraEscena()
+    {
+        FadeAndLoadScene(0);
+    }
+
+    /// <summary>
+    /// Carga la siguiente escena a partir de la escena actual.
+    /// </summary>
+    public void UltimaEscena()
+    {
+        fader.enabled = true;
+        FadeAndLoadScene(SceneManager.sceneCountInBuildSettings - 1);
+    }
+}
